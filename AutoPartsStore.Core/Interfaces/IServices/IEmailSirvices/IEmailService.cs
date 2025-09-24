7450fdd962f90b37a448531ef53d777060f6aac6
Azzam Omar Nasser Bajahaw <210280603@firat.edu.tr>
2025-09-24 15:56:06 +0300

    Create IEmailService.cs

---
+++ AutoPartsStore.Core/Interfaces/IServices/IEmailSirvices/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPartsStore.Core.Interfaces.IServices.IEmailSirvices
+{
+    internal interface IEmailService
+    {
+    }
+}
